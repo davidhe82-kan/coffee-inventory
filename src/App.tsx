12cb5475e4ce27b54c058968b25f5ab3c5313cc3
DavidHe <hekan82@hotmail.com>
2026-05-18 21:10:18 +0800

    fix: 使用HashRouter解决静态托管路由问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { InventoryPage } from '@/pages/InventoryPage'
 import { AddBeanPage } from '@/pages/AddBeanPage'
 import { BeanDetailPage } from '@/pages/BeanDetailPage'
@@ -6,14 +6,14 @@ import { EditBeanPage } from '@/pages/EditBeanPage'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<InventoryPage />} />
         <Route path="/add" element={<AddBeanPage />} />
         <Route path="/bean/:id" element={<BeanDetailPage />} />
         <Route path="/bean/:id/edit" element={<EditBeanPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
